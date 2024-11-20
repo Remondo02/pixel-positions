fbd47bfedad29f748074acd18adfeb5856629dd9
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-11-20 11:32:41 +0900

    Modification of the main view and components, main route and seed the DB

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,3 +1,5 @@
+@props(['job'])
+
 <x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo />
@@ -8,8 +10,8 @@
         <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
     </div>
     <div>
-        <x-tag>Job</x-tag>
-        <x-tag>Job</x-tag>
-        <x-tag>Job</x-tag>
+        @foreach ($job->tags as $tag)
+            <x-tag :$tag>Backend</x-tag>
+        @endforeach
     </div>
 </x-panel>
