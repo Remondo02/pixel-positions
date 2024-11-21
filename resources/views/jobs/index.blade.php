896276d97d23c19cb2e84c79319714a17e9eaa98
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-11-21 12:59:06 +0900

    Add basic search functionality

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,18 +2,17 @@
     <div class="space-y-10">
         <section class="text-center pt-6">
             <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
+            <x-forms.form action="/search" class="mt-6">
+                <x-forms.input :label="false" name="q" placeholder="Web Developer..."></x-forms.input>
+            </x-forms.form>
 
-            <form action="" class="mt-6">
-                <input type="text" placeholder="Web Developer..."
-                    class="rounded-xl bg-white/5 border border-white/10 px-5 py-4 w-full max-w-xl">
-            </form>
         </section>
         <section class="pt-10">
             <x-section-heading>Featured Jobs</x-section-heading>
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
                 @foreach ($featuredJobs as $job)
-                <x-job-card :$job />
-            @endforeach
+                    <x-job-card :$job />
+                @endforeach
             </div>
         </section>
         <section>
@@ -27,7 +26,6 @@
         <section>
             <x-section-heading>Recent Jobs</x-section-heading>
             <div class="mt-6 space-y-6">
-
                 @foreach ($jobs as $job)
                     <x-job-card-wide :$job />
                 @endforeach
